36857aecafd4f7834f5a566518ed49a051b03b92
Mbandi Awan <68437700+awanmbandi@users.noreply.github.com>
2024-10-18 19:16:15 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@
 # Terraform Settings Block
 # Hashicorp Google Registry: https://registry.terraform.io/providers/hashicorp/google/latest
 terraform {
-  required_version = "=> 1.1.8" # Terraform version constrain: This is the Required Terraform version while
+  required_version = "=> 1.9.8" # Terraform version constrain: This is the Required Terraform version while
   required_providers {          # Provider version constrain: This is the required provider version
     google = {
       source  = "hashicorp/google"
